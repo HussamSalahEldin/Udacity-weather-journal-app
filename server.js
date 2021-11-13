6f41613baea78ba69a8c57222b5b24e71e05dad8
HussamSalahEldin <92765259+HussamSalahEldin@users.noreply.github.com>
2021-11-13 18:28:28 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,41 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = [];
+
+// Require Express to run server and routes
+const express = require('express');
+
+// Start up an instance of app
+const app = express();
+
+const bodyParser = require('body-parser')
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+
+// Setup Server
+app.listen('8000', function() {
+    console.log('8000');
+})
+
+app.get('/all', function(req, res) {
+    res.send(projectData);
+})
+
+app.post('/add', function(req, res) {
+    project = {
+        temp: req.body.temp,
+        date: req.body.date,
+        feelings: req.body.feelings
+    };
+    projectData.push(project);
+    res.send(projectData)
+})
---
+++ website/app.js
@@ -0,0 +1,68 @@
+/* Global Variables */
+let baseURL = 'https://api.openweathermap.org/data/2.5/weather'
+const apiKey = 'c42bb84b10bfb0aa93e03d4c6db3ef72';
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+// Event listener
+document.getElementById('generate').addEventListener('click', performAction);
+
+function performAction(e) {
+    const zipCode = document.getElementById('zip').value;
+    const user_input = document.getElementById('feelings').value;
+
+    if (zipCode == '')
+        return;
+
+    const fullurl = `${baseURL}?q=${zipCode}&appid=${apiKey}`;
+
+    getData(fullurl)
+        .then(function (data) {
+            postWeatherData('/add', { temp: data.main.temp, date: newDate, feelings: user_input })
+        })
+        .then(function () {
+            updateUI()
+        });
+}
+
+// Async GET to call APIs using URL parameter
+// Make this generic to use in both (getting weather, and fetch all data in our app)
+const getData = async (url) => {
+    const response = await fetch(url);
+    try {
+        const newData = await response.json();
+        return newData
+    } catch (error) {
+        console.log("error", error);
+    }
+}
+
+
+// Async POST to add data in our array
+const postWeatherData = async (url = '', data = {}) => {
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data), // body data type must match "Content-Type" header        
+    });
+
+    try {
+        const newData = await response.json();
+        return newData
+    } catch (error) { 
+        console.log("error", error);
+    }
+}
+
+//Update UI function
+updateUI = async () => {
+    allData = await getData('/all');
+    document.getElementById('date').innerHTML = `My date: ${allData[allData.length - 1].date}`;
+    document.getElementById('temp').innerHTML = `My temp: ${allData[allData.length - 1].temp}`;
+    document.getElementById('content').innerHTML = `My feelings: ${allData[allData.length - 1].feelings}`;
+}
